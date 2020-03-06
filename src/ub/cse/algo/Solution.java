4947eda7d76628f94a2b497ed54c74111678a8e6
qiuhongl <qiuhongl@icloud.com>
2020-03-06 02:31:58 -0500

    accelerated a little bit

--- src/ub/cse/algo/Solution.java
+++ src/ub/cse/algo/Solution.java
@@ -13,29 +13,23 @@ public class Solution {
     public ArrayList<Integer> findCycle() {
         int[] Explored = new int[this.graph.size()];
         int[] Discovered = new int[this.graph.size()];
+        int[] parent = new int[this.graph.size()];
+        Arrays.fill(parent, -1);
         Stack<Integer> stack = new Stack<>();
 
         int startNode = 0;
         stack.push(startNode);
         Discovered[startNode] = 1;
 
-        HashMap<Integer, ArrayList<Integer>> pathCollection = new HashMap<>();
-        pathCollection.put(startNode, new ArrayList<>());
-
-        ArrayList<Integer> path = new ArrayList<>();
         ArrayList<Integer> cycle = new ArrayList<>();
 
-        int length = 0; // the length of the first cycle found
-
+        int lastNode = -1;
+        int firstNode = -1;
 
         // Use DFS algorithm to traverse the graph
         while (!stack.isEmpty()) {
 
             int node = stack.pop();
-
-            ArrayList<Integer> previousPath = pathCollection.get(node);  // get the previous path
-            int index = previousPath.size();
-            previousPath.add(node);
 
             boolean cycleFound = false;
 
@@ -43,33 +37,33 @@ public class Solution {
                 Explored[node] = 1;
 
                 // for each of the neighbor nodes, check whether it is explored
-                for (int adjNode : this.graph.get(node)) {
-                    boolean explored = Explored[adjNode] == 1;
-                    boolean discovered = Discovered[adjNode] == 1;
-
+                for (int childNode : this.graph.get(node)) {
+                    boolean explored = Explored[childNode] == 1;
+                    boolean discovered = Discovered[childNode] == 1;
+                    int parentNode = parent[node];
 
                     // if not explored and not discovered, mark it as discovered
                     if (!explored && !discovered) {
-                        stack.push(adjNode);
-                        Discovered[adjNode] = 1;
-                        ArrayList<Integer> currentPath = new ArrayList<>(previousPath);
-                        pathCollection.put(adjNode, currentPath); // Store the current path to the next node
+                        stack.push(childNode);
+                        Discovered[childNode] = 1;
+                        parent[childNode] = node;
                     }
 
                     // If discovered, update the path
                     if (!explored && discovered) {
-                        ArrayList<Integer> currentPath = new ArrayList<>(previousPath);
-                        pathCollection.put(adjNode, currentPath); // Store the current path to next node
+                        parent[childNode] = node;
                     }
 
                     // Check if it is explored just before the current node (as which could consider a 'parent' node)
                     // (Note: a cycle in an undirected graph needs to have at least three nodes in it.)
-                    if (explored && previousPath.get(index - 1) != adjNode) {
-                        int adjIndex = previousPath.indexOf(adjNode);
-                        length = index - adjIndex; // the length of the cycle;
-                        path = pathCollection.get(node);
-                        cycleFound = true;
-                        break;
+                    if (explored && parentNode != -1) {
+                        if (parentNode != childNode) {
+                            firstNode = childNode;
+                            lastNode = node;
+                            cycleFound = true;
+                            break;
+                        }
+
                     }
 
                 }
@@ -80,10 +74,13 @@ public class Solution {
             }
         }
 
-        int startIndex = path.size() - 1 - length;  // the length of the path minus the length of the cycle
+        while (lastNode != firstNode) {
+            cycle.add(lastNode);
+            lastNode = parent[lastNode];
+        }
 
-        for (int i = startIndex; i < path.size(); i++) {
-            cycle.add(path.get(i));
+        if (firstNode != -1) {
+            cycle.add(firstNode);
         }
 
         return cycle;
